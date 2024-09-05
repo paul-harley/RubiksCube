2a00850958afc61736675d5d86e84061b8d0e85d
paul-harley <paulharley2016@gmail.com>
2024-09-05 20:08:05 +0100

    edges drawn for overall cube. movement still not great

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
 
 #COLOURS
 WHITE = (255,255,255)
@@ -7,4 +7,39 @@ BLACK = (0,0,0)
 
 
 rotateAngle = 0
-cubeScale = 100
+cubeScale = 200
+
+#standard cube setup
+cubePoints = [
+    [[0,0,0]],
+    [[0,0,1]],
+    [[1,0,0]],
+    [[1,0,1]],
+    [[1,1,0]],
+    [[1,1,1]],
+    [[0,1,0]],
+    [[0,1,1]],
+]
+
+#GOING TO MAKE A SET OF INDICIES THAT A LINE SHOULD BE DRAWN BETWEEN
+#DISTANCE IS A GOOD WAY TO FIGUIRE IT OUT I THINK
+
+def findDis2Points3D(point1, point2):
+    difX = (point1[0][0] - point2[0][0]) * (point1[0][0] - point2[0][0])
+    difY = (point1[0][1] - point2[0][1]) * (point1[0][1] - point2[0][1])
+    difZ = (point1[0][2] - point2[0][2]) * (point1[0][2] - point2[0][2])
+
+    #technically for actual Distance this needs to be 
+    #square rooted but i just need which number is smaller 
+    #to draw a line between them - hypotenuse bigger doesnt need line
+    return difX + difY + difZ
+
+
+def linesToDrawIndicies():
+    linesToDraw = []
+    for i in range(len(cubePoints)):
+        for j in range(len(cubePoints)):
+            if findDis2Points3D(cubePoints[i], cubePoints[j]) == 1:
+                if [i,j] not in linesToDraw and [j,i] not in linesToDraw:
+                    linesToDraw.append([i,j])
+    return linesToDraw 
--- main.py
+++ main.py
@@ -4,34 +4,27 @@ import matrix
 
 screen = pygame.display.set_mode((globals.SCREEN_HEIGHT, globals.SCREEN_WIDTH))
 
-cubePoints = [
-    [[0,0,0]],
-    [[0,0,1]],
-    [[1,0,0]],
-    [[1,0,1]],
-    [[1,1,0]],
-    [[1,1,1]],
-    [[0,1,0]],
-    [[0,1,1]],
-]
-
+bigCubePoints = globals.cubePoints
 cube2DPoints = []
+lineToDraw = globals.linesToDrawIndicies()
+print(lineToDraw)
 
-for p in cubePoints:
+for p in bigCubePoints:
     for i in range(3):
         p[0][i] = p[0][i] * globals.cubeScale
-        
-        #just a random num so its fully on screen
-        #will centre in future
-        p[0][i] += 70
 
 
 def update2DCords():
     cube2DPoints.clear()
-    for p in cubePoints:
+    for p in bigCubePoints:
         cube2DPoints.append(matrix.multiplyMatrix(p, matrix.orthographicProjectionMatrix))
+    
+    for p in cube2DPoints:
+        p[0][0] += globals.SCREEN_WIDTH/2
+        p[0][1] += globals.SCREEN_HEIGHT/4
 
 update2DCords()
+print(cube2DPoints)
 
 
 clock = pygame.time.Clock()
@@ -40,33 +33,51 @@ while resume:
 
     key = pygame.key.get_pressed()
 
+    #ROTATING OVERALL CUBE
     if key[pygame.K_w]: #UP
+        if key[pygame.K_d]: #RIGHT
+                globals.rotateAngle = 0.05
+                bigCubePoints = matrix.rotateCube(bigCubePoints, 'z', globals.rotateAngle)
+                update2DCords()
         globals.rotateAngle = -0.05
-        cubePoints = matrix.rotateCube(cubePoints, 'x', globals.rotateAngle)
+        bigCubePoints = matrix.rotateCube(bigCubePoints, 'x', globals.rotateAngle)
         update2DCords()
-        print(cube2DPoints)
 
     if key[pygame.K_s]: #DOWN
+        if key[pygame.K_a]: #LEFT
+            globals.rotateAngle = 0.05
+            bigCubePoints = matrix.rotateCube(bigCubePoints, 'z', globals.rotateAngle)
+            update2DCords()
         globals.rotateAngle = 0.05
-        cubePoints = matrix.rotateCube(cubePoints, 'x', globals.rotateAngle)
+        bigCubePoints = matrix.rotateCube(bigCubePoints, 'x', globals.rotateAngle)
         update2DCords()
-        print(cube2DPoints)
 
     if key[pygame.K_a]: #LEFT
         globals.rotateAngle = 0.05
-        cubePoints = matrix.rotateCube(cubePoints, 'y', globals.rotateAngle)
+        bigCubePoints = matrix.rotateCube(bigCubePoints, 'y', globals.rotateAngle)
         update2DCords()
-        print(cube2DPoints)
 
     if key[pygame.K_d]: #RIGHT
         globals.rotateAngle = -0.05
-        cubePoints = matrix.rotateCube(cubePoints, 'y', globals.rotateAngle)
+        bigCubePoints = matrix.rotateCube(bigCubePoints, 'y', globals.rotateAngle)
         update2DCords()
-        print(cube2DPoints)
-
+    
+    #DRAWING BIG CUBE
     screen.fill(globals.BLACK)
+    #verticies
     for p in cube2DPoints:
         pygame.draw.circle(screen, globals.WHITE, (p[0][0], p[0][1]), 5)
+    
+    #edges
+    for i in range(len(lineToDraw)):
+        xCordStart = cube2DPoints[lineToDraw[i][0]][0][0]
+        yCordStart = cube2DPoints[lineToDraw[i][0]][0][1]
+
+        xCordEnd = cube2DPoints[lineToDraw[i][1]][0][0]
+        yCordEnd = cube2DPoints[lineToDraw[i][1]][0][1]
+
+        pygame.draw.line(screen, globals.WHITE, [xCordStart, yCordStart], [xCordEnd, yCordEnd])
+        
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
